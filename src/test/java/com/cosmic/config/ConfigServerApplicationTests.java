24ea4d19ef5f2bcc9e7ba06aee1b5e868494c7e6
Danitha Dinuwan <danithahk@gmail.com>
2026-03-28 10:05:49 +0530

    update

---
+++ src/test/java/com/cosmic/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmic.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
